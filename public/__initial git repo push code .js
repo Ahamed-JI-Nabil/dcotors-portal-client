1bc0c7bde3337d79f2adf9c0c919429e25910d5c
NABIL <johayeriqbal@gmail.com>
2022-05-22 01:22:42 +0600

    first commit

---
+++ public/__initial git repo push code .js
@@ -0,0 +1,9 @@
+/*
+echo "# doctors-portal-server" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ahamed-JI-Nabil/doctors-portal-server.git
+git push -u origin main
+*/
--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/Pages/About/About';
+import Home from './Components/Pages/Home/Home';
+import Login from './Components/Pages/Login/Login/Login';
+import Navbar from './Components/Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
